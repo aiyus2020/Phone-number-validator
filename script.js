55c15dd9378c46543c1721f383f5159198851dfb
aiyus <aiyusboss@gmail.com>
2024-09-06 14:08:03 +0100

    second commit

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ const clearBtn = document.getElementById("clear-btn");
 let result = document.getElementById("results-div");
 
 const valPhoneNumber = () => {
-  const userInput = inputs.value;
-  const regex = /^(?:\+234\s?)?(\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/;
+  const userInput = inputs.value.trim(); // Trim any spaces around the input
+  const regex =
+    /^(?:\+234|0)?([\s-]?)(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/;
+
+  // Check if input is empty
   if (userInput === "") {
     alert("Please provide a phone number");
     return;
   }
 
+  // Test against the regex pattern and display result
   if (regex.test(userInput)) {
     result.innerHTML += `<li class='text'>Valid Nigeria number: ${userInput}</li>`;
   } else {
@@ -23,7 +27,7 @@ checkBtn.addEventListener("click", () => {
   valPhoneNumber();
   inputs.value = ""; // Clear the input after checking
 });
-``;
+
 clearBtn.addEventListener("click", () => {
   result.innerHTML = ""; // Clear the result display
 });
